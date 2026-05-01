e9b12fa568175f1fd9e4a8a748e7abbda5a318eb
T3M8029 <102697658+T3M8029@users.noreply.github.com>
2026-05-01 18:33:10 +0200

    SL jobs shouldn´t reserve space on L tracks, partial fix for #10

---
+++ PersistentJobsMod/HarmonyPatches/JobChainControllers/StaticShuntingLoadJobDefinition_Patch.cs
@@ -0,0 +1,24 @@
+﻿using DV.Logic.Job;
+using HarmonyLib;
+using PersistentJobsMod.Utilities;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace PersistentJobsMod.HarmonyPatches.JobChainControllers
+{
+
+    [HarmonyPatch(typeof(StaticShuntingLoadJobDefinition), nameof(StaticShuntingLoadJobDefinition.GetRequiredTrackReservations))]
+    public static class StaticShuntingLoadJobDefinition_Patch
+    {
+        private static readonly FieldInfo _TrackIDTrackType = ReflectionUtilities.CompatAccess.Field(typeof(TrackID), "trackType");
+
+        public static void Postfix(ref List<TrackReservation> __result, Track ___destinationTrack)
+        {
+            if (__result != null && ((string)(_TrackIDTrackType.GetValue(___destinationTrack.ID)) == "L"))
+            {
+                Main._modEntry.Logger.Log("SL job prevented from reserving space on " + __result[0].track.ID.FullDisplayID);
+                __result = new List<TrackReservation>();
+            }
+        }
+    }
+}
