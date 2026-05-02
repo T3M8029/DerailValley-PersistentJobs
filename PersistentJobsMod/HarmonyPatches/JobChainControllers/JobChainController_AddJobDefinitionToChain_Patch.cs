6a92ceb0d8bd19936aa06528ca0d85218ffefab6
T3M8029 <102697658+T3M8029@users.noreply.github.com>
2026-05-02 16:42:10 +0200

    Reserve track space only on job acceptance - resolves #10

---
+++ PersistentJobsMod/HarmonyPatches/JobChainControllers/JobChainController_AddJobDefinitionToChain_Patch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace PersistentJobsMod.HarmonyPatches.JobChainControllers
+{
+    [HarmonyPatch(typeof(JobChainController), nameof(JobChainController.AddJobDefinitionToChain))]
+    public static class JobChainController_AddJobDefinitionToChain_Patch
+    {
+        //the dict will get populated only when the reservation actually gets made, for now remove this "promise"
+        public static void Postfix(StaticJobDefinition jobInChain, JobChainController __instance)
+        {
+            __instance.jobDefToCurrentlyReservedTracks[jobInChain] = new();
+        }
+    }
+}
